a09f869f6a447add6fa8d264852993f5087dc3ff
PeterDeWeirdt <petedeweirdt@gmail.com>
2018-11-07 16:07:12 -0500

    bug (raw data): removed EEF2

--- guide_design/tasks/get_data/__init__.py
+++ guide_design/tasks/get_data/__init__.py
@@ -25,12 +25,3 @@ class RS2model(luigi.ExternalTask):
     def output(self):
         return luigi.LocalTarget('./data/RS2_outputs')
 
-class Cpf1_A375_Controls(luigi.ExternalTask):
-    __version__ = '0.1'
-    def output(self):
-        return luigi.LocalTarget('./data/A375_EEF2_controls.csv')
-
-class Cpf1_A375_Control_Context(luigi.ExternalTask):
-    __version__ = '0.1'
-    def output(self):
-        return luigi.LocalTarget('./data/EEF2_control_context_output.csv')
